5ede312d4f9daf6a33bb1ae3356118c7bf8df0b2
rva-mike <mike.levy.rva@gmail.com>
2023-03-04 16:15:23 -0500

    components created and imported into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import styles from './style'
+import styles from './style';
+
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
+
 
 const App = () => (
 
