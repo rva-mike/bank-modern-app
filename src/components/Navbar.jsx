ebd9177a59649d58e8f5847e161e7b7dbbbd57ff
rva-mike <mike.levy.rva@gmail.com>
2023-03-06 22:03:53 -0500

    navbar mobile menu logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,12 @@ import { useState } from 'react'
 import { close, logo, menu } from '../assets'
 import { navLinks } from '../constants'
 
-// logic for line 16; dynamic block, if index is last element of navLinks then no margin, if else, margin right 10
+// logic for ${index === navLinks.length -1 ? 'mr-0' : 'mr-10'}; dynamic block, if index is last element of navLinks then no margin, if else, margin right 10
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
       <img src={logo} alt="hoobank" className='w-[124px] h-[32px]' />
@@ -20,6 +23,18 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        {/* if toggle is turned on show closed icons, otherwise show menu icon  */}
+        <img 
+        className='w-[28px] h-[28px] object-contain'
+        // callback function; in react never update the state by using the previous version of that same old state. If you want to change the state using the previous version of that same state, then create call back function using for ex. prev and !prev. This way react makes sure it keeps the state up to date
+        onClick={()=> setToggle((prev) => !prev)}
+        src={toggle ? close : menu} alt="menu" />
+
+        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`} ></div>
+
+      </div>
 
     </nav>
   )
