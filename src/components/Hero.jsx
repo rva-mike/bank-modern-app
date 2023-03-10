6b8f1d7398048ae2d7e5348c030e40643ea01503
rva-mike <mike.levy.rva@gmail.com>
2023-03-09 20:49:00 -0500

    final touches for hero, gradient finish, getstarted component positioning for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => (
       </div>
 
       <div className="flex flex-row justify-between items-center w-full">
-        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px text-white ss:leading-[100px] leading-[75px]">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
           {/* break class = on small devices exist, usually hidden  */}
           The Next <br className="sm:block hidden" /> {" "}
           <span className="text-gradient">Generation</span> {" "}
@@ -23,17 +23,27 @@ const Hero = () => (
           <GetStarted />
         </div>
       </div>
-      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px text-white ss:leading-[100px] leading-[75px] w-full">Payment Method</h1>
+      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">Payment Method.</h1>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
     </div>
 
-    <div>
+    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
       <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
-      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>
+      {/* gradient start */}
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+      <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+      {/* gradient end */}
     </div>
 
-  </section>
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+      <GetStarted />
+    </div>
+
+
+
+  </section >
 )
 
 
